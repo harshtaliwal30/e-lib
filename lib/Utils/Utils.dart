010cc7c92fc88aaed08a9c44d01fef39459ac03d
harshtaliwal30 <harshtaliwal@gmail.com>
2021-08-16 18:00:37 +0530

    Added theme and removed dead code

---
+++ lib/Utils/Utils.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Utils {
+  static const lightBgColor = Color(0XFFEFF3F6);
+  static const fillColor = Color(0XFFEFF3F6);
+  static const green = Color(0xFF66B032);
+  static const aliceBlue = Color(0xFFCDEEFF);
+  static const antiFlashWhite = Color(0XFFF3F3F3);
+  static const bondiBlue = Color(0XFF4293B7);
+  static const white = Color(0xFFF2F2F2);
+  static const pureWhite = Color(0xFFFFFFFF);
+  static const red = Color(0xFFFF406C);
+  static const grey = Color(0xFF9B9B9B);
+  static const greyLight = Color(0xFFE0E0E0);
+  static const greyDark = Color(0xFF353B48);
+  static const black = Color(0xFF262628);
+  static const black54 = Colors.black54;
+  static const blackLight = Color(0xFF2B2F3A);
+  static const blue = Color(0xFF00A2FE);
+  static const blue_dark = Color(0xFF00358D);
+  static const inactive = Color(0xFF404040);
+  static const parrot = Color(0XFF7CE247);
+  static const errorColor = Color(0xFFFF0000);
+}

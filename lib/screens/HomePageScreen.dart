aee01fd5872da7385a5041249df110bcd5af358f
harshtaliwal30 <harshtaliwal@gmail.com>
2021-08-17 16:17:29 +0530

    Added search bar UI

--- lib/screens/HomePageScreen.dart
+++ lib/screens/HomePageScreen.dart
@@ -4,6 +4,7 @@ import 'package:e_lib/Utils/SizeConfig.dart';
 import 'package:e_lib/Utils/Utils.dart';
 import 'package:e_lib/helper/UIHelper.dart';
 import 'package:e_lib/models/LibraryModel.dart';
+import 'package:e_lib/screens/LibraryPageScreen.dart';
 import 'package:flutter/material.dart';
 
 class HomePageScreen extends StatefulWidget {
@@ -14,12 +15,6 @@ class HomePageScreen extends StatefulWidget {
 }
 
 class _HomePageScreenState extends State<HomePageScreen> {
-  List<Color> colors = [
-    Color(0XFFFE8A65),
-    Color(0XFF30AC70),
-    Color(0XFF566E7A),
-    Color(0XFF0177BB)
-  ];
   List<LibraryModel> libraries = [];
 
   @override
@@ -39,7 +34,7 @@ class _HomePageScreenState extends State<HomePageScreen> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      backgroundColor: Utils.pureWhite,
+      backgroundColor: Utils.lightBgColor,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50),
         child: UIHelper.getAppBarView(
@@ -50,9 +45,65 @@ class _HomePageScreenState extends State<HomePageScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: SizeConfig.safeBlockHorizontal * 4,
+                    ),
+                    margin: EdgeInsets.only(
+                      left: SizeConfig.safeBlockHorizontal * 2,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Utils.pureWhite,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: TextField(
+                      autofocus: false,
+                      onSubmitted: (value) {
+                        // if (value.length > 3) {
+                        //   setState(() {
+                        //     recentSearchList.add(value);
+                        //     savedRecentSearch(value);
+                        //   });
+                        //   preformSearch(value);
+                        // }
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Search for libraries',
+                        hintStyle: TextStyle(
+                          fontSize: SizeConfig.safeBlockHorizontal * 4,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () async {},
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      right: SizeConfig.safeBlockHorizontal * 2,
+                    ),
+                    padding: EdgeInsets.only(
+                      left: SizeConfig.safeBlockHorizontal * 4,
+                    ),
+                    child: Icon(Icons.search),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: SizeConfig.safeBlockHorizontal * 6,
+            ),
             ListView.builder(
               scrollDirection: Axis.vertical,
               shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
               itemCount: libraries.length,
               itemBuilder: (BuildContext context, int index) {
                 return getLibraryView(
@@ -67,93 +118,107 @@ class _HomePageScreenState extends State<HomePageScreen> {
   }
 
   Widget getLibraryView(int index) {
-    return Card(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(10),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LibraryPageScreen(
+              libraryModel: libraries[index],
+            ),
+          ),
+        );
+      },
+      child: Card(
+        shadowColor: Utils.aliceBlue,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          side: BorderSide(
+            color: Utils.black,
+            width: 0.5,
+          ),
         ),
-        side: BorderSide(
-          color: Utils.greyLight,
-          width: 0.5,
+        margin: EdgeInsets.only(
+          left: SizeConfig.safeBlockHorizontal * 2,
+          right: SizeConfig.safeBlockHorizontal * 2,
+          bottom: SizeConfig.safeBlockHorizontal * 2,
         ),
-      ),
-      margin: EdgeInsets.only(
-        left: SizeConfig.safeBlockHorizontal * 2,
-        right: SizeConfig.safeBlockHorizontal * 2,
-        bottom: SizeConfig.safeBlockHorizontal * 2,
-      ),
-      child: Container(
-        padding: EdgeInsets.all(
-          SizeConfig.safeBlockHorizontal * 2,
-        ),
-        height: SizeConfig.screenHeight / 6,
-        child: Row(
-          children: [
-            Container(
-              height: SizeConfig.screenHeight / 6,
-              width: 100,
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-                child: CachedNetworkImage(
-                  fit: BoxFit.fill,
-                  imageUrl: libraries[index].libraryImage!,
-                  errorWidget: (context, url, error) => Icon(Icons.error),
+        child: Container(
+          padding: EdgeInsets.all(
+            SizeConfig.safeBlockHorizontal * 2,
+          ),
+          height: SizeConfig.screenHeight / 6,
+          child: Row(
+            children: [
+              Container(
+                height: SizeConfig.screenHeight / 6,
+                width: SizeConfig.screenWidth / 4,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: CachedNetworkImage(
+                    fit: BoxFit.fill,
+                    imageUrl: libraries[index].libraryImage!,
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: SizeConfig.safeBlockHorizontal * 3,
-                      top: SizeConfig.safeBlockHorizontal * 4,
-                    ),
-                    child: Text(
-                      libraries[index].libraryName!,
-                      overflow: TextOverflow.ellipsis,
-                      softWrap: true,
-                      style: TextStyle(
-                        color: Utils.black,
-                        fontSize: SizeConfig.safeBlockHorizontal * 4.5,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: SizeConfig.safeBlockHorizontal * 3,
+                        top: SizeConfig.safeBlockHorizontal * 4,
+                      ),
+                      child: Text(
+                        libraries[index].libraryName!,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: true,
+                        style: TextStyle(
+                          color: Utils.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: SizeConfig.safeBlockHorizontal * 4.5,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: SizeConfig.safeBlockHorizontal * 3,
-                      // top: SizeConfig.safeBlockHorizontal * 4,
-                    ),
-                    child: Text(
-                      libraries[index].city!,
-                      style: TextStyle(
-                        color: Utils.black,
-                        fontSize: SizeConfig.safeBlockHorizontal * 4,
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: SizeConfig.safeBlockHorizontal * 3,
+                        top: SizeConfig.safeBlockHorizontal * 1,
+                      ),
+                      child: Text(
+                        libraries[index].city!,
+                        style: TextStyle(
+                          color: Utils.black,
+                          fontSize: SizeConfig.safeBlockHorizontal * 4,
+                        ),
                       ),
                     ),
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: SizeConfig.safeBlockHorizontal * 3,
-                      bottom: SizeConfig.safeBlockHorizontal * 2,
-                    ),
-                    child: Text(
-                      libraries[index].type!,
-                      style: TextStyle(
-                        color: Utils.black,
-                        fontSize: SizeConfig.safeBlockHorizontal * 4,
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: SizeConfig.safeBlockHorizontal * 3,
+                        bottom: SizeConfig.safeBlockHorizontal * 2,
                       ),
-                    ),
-                  )
-                ],
+                      child: Text(
+                        libraries[index].type!,
+                        style: TextStyle(
+                          color: Utils.black,
+                          fontSize: SizeConfig.safeBlockHorizontal * 4,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/LibraryPageScreen.dart
@@ -0,0 +1,116 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_lib/Utils/SizeConfig.dart';
+import 'package:e_lib/Utils/Utils.dart';
+import 'package:e_lib/helper/UIHelper.dart';
+import 'package:e_lib/models/LibraryModel.dart';
+import 'package:flutter/material.dart';
+
+class LibraryPageScreen extends StatefulWidget {
+  const LibraryPageScreen({
+    Key? key,
+    required this.libraryModel,
+  }) : super(key: key);
+
+  final LibraryModel libraryModel;
+
+  @override
+  _LibraryPageScreenState createState() => _LibraryPageScreenState();
+}
+
+class _LibraryPageScreenState extends State<LibraryPageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Utils.pureWhite,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: UIHelper.getAppBarView(
+          context,
+          widget.libraryModel.libraryName!,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Utils.lightBgColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
+              padding: EdgeInsets.all(
+                SizeConfig.safeBlockHorizontal * 2,
+              ),
+              child: Card(
+                color: Utils.lightBgColor,
+                elevation: 0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: SizeConfig.screenHeight / 3.5,
+                      width: double.infinity,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        child: CachedNetworkImage(
+                          fit: BoxFit.fill,
+                          imageUrl: widget.libraryModel.libraryImage!,
+                          errorWidget: (context, url, error) =>
+                              Icon(Icons.error),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: SizeConfig.safeBlockHorizontal * 2,
+                        top: SizeConfig.safeBlockHorizontal * 4,
+                      ),
+                      child: Text(
+                        widget.libraryModel.libraryName!,
+                        softWrap: true,
+                        style: TextStyle(
+                          color: Utils.black,
+                          fontSize: SizeConfig.safeBlockHorizontal * 4.5,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: SizeConfig.safeBlockHorizontal * 2,
+                        top: SizeConfig.safeBlockHorizontal * 1,
+                      ),
+                      child: Text(
+                        widget.libraryModel.type!,
+                        style: TextStyle(
+                          color: Utils.black,
+                          fontSize: SizeConfig.safeBlockHorizontal * 4,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: SizeConfig.safeBlockHorizontal * 2,
+                        top: SizeConfig.safeBlockHorizontal * 1,
+                      ),
+                      child: Text(
+                        widget.libraryModel.city!,
+                        style: TextStyle(
+                          color: Utils.black,
+                          fontSize: SizeConfig.safeBlockHorizontal * 4,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

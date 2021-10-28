5465246a4c5fb51b024710537ff00f8cf67f2cf2
harshtaliwal30 <harshtaliwal@gmail.com>
2021-10-29 01:01:04 +0530

    Book listview UI

--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -30,6 +30,7 @@ class LibraryScreen extends StatelessWidget {
       body: Obx(
         () => SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 padding: EdgeInsets.all(
@@ -37,7 +38,7 @@ class LibraryScreen extends StatelessWidget {
                 ),
                 child: Card(
                   color: Utils.primaryColor,
-                  elevation: 15,
+                  elevation: 0,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
@@ -53,7 +54,9 @@ class LibraryScreen extends StatelessWidget {
                           ),
                           child: CachedNetworkImage(
                             fit: BoxFit.cover,
-                            imageUrl: libraryModel.libraryImage!,
+                            // imageUrl: libraryModel.libraryImage!,
+                            imageUrl:
+                                "https://firebasestorage.googleapis.com/v0/b/e-lib-e53d6.appspot.com/o/28828394_950086555154619_9137006216337597187_o.jpg?alt=media&token=dd2c5145-1e19-43d6-ace7-6bede86f7a7d",
                             errorWidget: (context, url, error) => Icon(Icons.error),
                           ),
                         ),
@@ -115,6 +118,15 @@ class LibraryScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              Utils().getWithPadding(
+                Utils().getText(
+                  "All Books",
+                  fontWeight: FontWeight.bold,
+                ),
+                left: SizeConfig.safeBlockHorizontal * 3.5,
+                top: SizeConfig.safeBlockVertical * 2,
+                bottom: SizeConfig.safeBlockVertical * 2,
+              ),
               ListView.builder(
                 itemCount: _libraryScreenController.booksDataList.length,
                 shrinkWrap: true,
@@ -135,36 +147,33 @@ class LibraryScreen extends StatelessWidget {
         right: SizeConfig.safeBlockHorizontal * 2,
         bottom: SizeConfig.safeBlockHorizontal * 3,
       ),
-      child: Stack(
-        children: [
-          Card(
-            shadowColor: Utils.white,
-            elevation: 15,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-            ),
-            margin: EdgeInsets.only(
-              right: SizeConfig.safeBlockHorizontal * 3.5,
-            ),
-            child: Container(
+      child: Card(
+        shadowColor: Utils.white,
+        elevation: 15,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
               height: SizeConfig.screenHeight / 5.5,
               child: Row(
                 children: [
-                  Container(
-                    height: SizeConfig.screenHeight / 5.5,
-                    width: SizeConfig.screenWidth / 4,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                      ),
-                      child: CachedNetworkImage(
-                        fit: BoxFit.fill,
-                        imageUrl:
-                            "https://firebasestorage.googleapis.com/v0/b/e-lib-e53d6.appspot.com/o/28828394_950086555154619_9137006216337597187_o.jpg?alt=media&token=dd2c5145-1e19-43d6-ace7-6bede86f7a7d",
-                        errorWidget: (context, url, error) => Icon(Icons.error),
+                  Padding(
+                    padding: EdgeInsets.only(left: SizeConfig.safeBlockHorizontal * 3),
+                    child: Container(
+                      height: SizeConfig.screenHeight / 6.5,
+                      width: SizeConfig.screenWidth / 3.5,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        child: CachedNetworkImage(
+                          fit: BoxFit.fill,
+                          imageUrl:
+                              "https://firebasestorage.googleapis.com/v0/b/e-lib-e53d6.appspot.com/o/28828394_950086555154619_9137006216337597187_o.jpg?alt=media&token=dd2c5145-1e19-43d6-ace7-6bede86f7a7d",
+                          errorWidget: (context, url, error) => Icon(Icons.error),
+                        ),
                       ),
                     ),
                   ),
@@ -269,58 +278,43 @@ class LibraryScreen extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-          Positioned(
-            right: 0,
-            top: 0,
-            child: Column(
-              children: [
-                InkWell(
-                  onTap: () async {
-                    // _libraryScreenController.editBookBottomSheet(index);
-                  },
-                  child: CircleAvatar(
-                    radius: 15,
-                    backgroundColor: Utils.blue,
-                    child: Icon(
-                      Icons.edit,
-                      size: SizeConfig.iconExamHeightAndWidth * 0.5,
-                      color: Utils.white,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                InkWell(
-                  onTap: () async {
-                    var result = await Utils().showDialog(
-                      "Alert",
-                      "Are you sure you want to delete the book?",
-                      () {
-                        // _libraryScreenController.deleteBook(_libraryScreenController.booksDataList[index].bookDocId);
-                        Get.back(result: "deleted");
-                      },
-                    );
-                    if (result != null && result == "deleted") {
-                      Utils().showConfirmSnackbar("Book deleted successfully");
-                      _libraryScreenController.booksDataList.removeAt(index);
-                    }
-                  },
-                  child: CircleAvatar(
-                    radius: 15,
-                    backgroundColor: Utils.red,
-                    child: Icon(
-                      Icons.delete,
-                      size: SizeConfig.iconExamHeightAndWidth * 0.5,
-                      color: Utils.white,
-                    ),
-                  ),
-                ),
-              ],
+            SizedBox(height: 10),
+            Divider(
+              height: 0,
+              color: Utils.grey,
+              indent: 10,
+              endIndent: 10,
+              thickness: 0.5,
             ),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: SizeConfig.safeBlockHorizontal * 2,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: MaterialButton(
+                      onPressed: () async {},
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                      ),
+                      child: Utils().getText(
+                        "Issue Book",
+                        color: Utils.green,
+                        fontWeight: FontWeight.bold,
+                        fontSize: SizeConfig.baseFontSize * 3.5,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
